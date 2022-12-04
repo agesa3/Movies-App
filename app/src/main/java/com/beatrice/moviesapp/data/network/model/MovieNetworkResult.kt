2ac0bfe2ae7e7f7fdbfea272ef703835d0524a9b
agesa3 <collinsagesa2000@gmail.com>
2022-12-04 17:10:58 +0300

    refacored handling of nullability checks

--- app/src/main/java/com/beatrice/moviesapp/data/network/model/MovieNetworkResult.kt
+++ app/src/main/java/com/beatrice/moviesapp/data/network/model/MovieNetworkResult.kt
@@ -1,8 +1,8 @@
 package com.beatrice.moviesapp.data.network.model
 
 data class MovieNetworkResult(
-    val page: Int,
-    val results: List<Result>,
-    val total_pages: Int,
-    val total_results: Int
+    val page: Int?,
+    val results: List<Result>?,
+    val total_pages: Int?,
+    val total_results: Int?
 )
--- app/src/main/java/com/beatrice/moviesapp/domain/model/Movie.kt
+++ app/src/main/java/com/beatrice/moviesapp/domain/model/Movie.kt
@@ -7,34 +7,35 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Movie(
     val id: Int,
-    val title: String?="",
-    val originalTitle: String?= "",
-    val language: String?= "",
-    val releaseDate: String?= "",
-    val voteCount: Int?= 0,
-    val voteAverage: Double?= 0.0,
-    val popularity: Double?= 0.0,
-    val posterPath: String?= "",
-    val backdropPath: String?= "",
-    val overview: String?= "",
-): Parcelable
+    val title: String,
+    val originalTitle: String,
+    val language: String,
+    val releaseDate: String,
+    val voteCount: Int,
+    val voteAverage: Double,
+    val popularity: Double,
+    val posterPath: String,
+    val backdropPath: String,
+    val overview: String,
+) : Parcelable
 
-fun MovieNetworkResult.toMoviesList(): List<Movie> =results.map {
+fun MovieNetworkResult.toMoviesList(): List<Movie> {
+    return results?.map {
         Movie(
-            id = it.id,
-            title = it.title,
-            originalTitle = it.original_title,
-            language = it.original_language,
-            releaseDate = it.release_date,
-            voteAverage = it.vote_average,
-            voteCount = it.vote_count,
-            popularity = it.popularity,
-            posterPath = it.poster_path,
-            backdropPath = it.backdrop_path,
-            overview = it.overview
-
+            id = it.id ?: 0,
+            title = it.title ?: "",
+            originalTitle = it.original_title ?: "",
+            language = it.original_language ?: "",
+            releaseDate = it.release_date ?: "",
+            voteCount = it.vote_count ?: 0,
+            voteAverage = it.vote_average ?: 0.0,
+            popularity = it.popularity ?: 0.0,
+            posterPath = it.poster_path ?: "",
+            backdropPath = it.backdrop_path ?: "",
+            overview = it.overview ?: ""
         )
-    }.toList()
+    } ?: emptyList()
+}
 
 
 
