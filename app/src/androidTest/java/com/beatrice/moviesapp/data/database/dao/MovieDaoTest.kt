4b68ded7374f0040d6606dc09bf8f919ed424928
kanake10 <10kanake@gmail.com>
2022-12-11 21:01:31 +0300

    movie dao tests passing 🎉

---
+++ app/src/androidTest/java/com/beatrice/moviesapp/data/database/dao/MovieDaoTest.kt
@@ -0,0 +1,59 @@
+package com.beatrice.moviesapp.data.database.dao
+
+import androidx.room.Room
+import com.beatrice.moviesapp.data.database.MoviesDatabase
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+
+import androidx.test.platform.app.InstrumentationRegistry
+import com.beatrice.moviesapp.resources.fakeMovieList
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert
+import org.junit.Test
+
+class MovieDaoTest {
+
+    private lateinit var database: MoviesDatabase
+    private lateinit var movieDao: MovieDao
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        database = Room.inMemoryDatabaseBuilder(context, MoviesDatabase::class.java)
+            .allowMainThreadQueries().build()
+        movieDao = database.movieDao()
+    }
+
+    @After
+    fun tearDown() {
+        runBlocking {
+            movieDao.deleteMovies()
+            database.close()
+        }
+    }
+
+    @Test
+    fun insertMovies() {
+        runBlocking {
+            val moviesTestFake = fakeMovieList
+
+            val reposTestListUtil = listOf(moviesTestFake)
+            movieDao.insertMovies(reposTestListUtil)
+
+            val result = movieDao.getMovies()
+            Assert.assertEquals(reposTestListUtil, result)
+        }
+    }
+
+    @Test
+    fun deleteMovies() {
+        runBlocking {
+            movieDao.insertMovies(listOf(fakeMovieList))
+            movieDao.deleteMovies()
+            val usersFollowers = movieDao.getMovies()
+            assertThat(usersFollowers.isEmpty())
+        }
+    }
+
+}
