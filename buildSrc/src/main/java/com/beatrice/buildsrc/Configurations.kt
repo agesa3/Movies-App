d128717124f336f9a24ca2478e680e91272cda33
Beatrice Kinya <gitongakinya1@gmail.com>
2023-01-27 12:39:27 +0300

    start migrating dependencies

---
+++ buildSrc/src/main/java/com/beatrice/buildsrc/Configurations.kt
@@ -0,0 +1,11 @@
+package com.beatrice.buildsrc
+
+object ConfigurationData {
+    const val applicationId = "com.beatrice.moviesapp"
+    const val compileSdk = 33
+//    const val buildToolsVersion = "30.0.3"
+    const val minSdk = 22
+    const val targetSdk= 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ buildSrc/src/main/java/com/beatrice/buildsrc/Dependencies.kt
@@ -0,0 +1,61 @@
+package com.beatrice.buildsrc
+
+object Versions {
+    val compose = "1.4.0-alpha03"
+    val material3 = "1.1.0-alpha03"
+    val constraintLayout = "1.0.1"
+    val navigation = "2.5.3"
+    val lifecycle = "2.6.0-alpha03"
+    val coroutine = "1.6.4"
+    val daggerHilt = "2.44.2"
+    val hiltCompiler = "1.0.0"
+    val hiltNavigationCompose = "1.0.0"
+    val hiltWork = "1.0.0"
+    val logcat = "0.1"
+    val coil = "2.2.2"
+    val jUnit4 = "4.13.2"
+}
+
+object Libraries {
+    // Compose
+    val activityCompose = "androidx.activity:activity-compose:${Versions.compose}"
+    val composeUi = "androidx.compose.ui:ui:~${Versions.compose}"
+    val composePreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
+
+    // Material 3
+    val material3 = "androidx.compose.material3:material3: ${Versions.material3}"
+    val constraintLayout =
+        "androidx.constraintlayout:constraintlayout-compose:${Versions.constraintLayout}"
+
+    // Navigation
+    val navigation = "androidx.navigation:navigation-compose:${Versions.navigation}"
+
+    // Lifecycle
+    val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
+    val viewModelSavedState = "androidx.lifecycle:lifecycle-viewmodel-savedstate:${Versions.lifecycle}"
+    val lifecycleRuntimeCompose = "androidx.lifecycle:lifecycle-runtime-compose:${Versions.lifecycle}"
+
+    // Coroutines
+    val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutine}"
+
+    // Hilt
+    val daggerHilt = "com.google.dagger:hilt-android:${Versions.daggerHilt}"
+    val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose:${Versions.hiltNavigationCompose}"
+    val hiltWork = "androidx.hilt:hilt-work:${Versions.hiltWork}"
+
+    // Coil
+    val coil = "io.coil-kt:coil-compose:${Versions.coil}"
+
+    // logcat
+    val logcat = "com.squareup.logcat:logcat:${Versions.logcat}"
+
+
+}
+
+object TestLibraries{
+    val junit4 = "unit:junit:${Versions.jUnit4}"
+    val composeUi = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
+    val composeTooling = "androidx.compose.ui:ui-tooling:${Versions.compose}"
+    val composeManifest = "androidx.compose.ui:ui-test-manifest:${Versions.compose}"
+    val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutine}"
+}
